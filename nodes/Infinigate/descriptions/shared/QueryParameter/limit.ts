282dd69fded6d914a4b4d0a19c97fb766451a8d0
affeldt28 <code@affeldt.dev>
2026-06-29 15:53:43 +0200

    fix: limit been applied even if not set

--- nodes/Infinigate/descriptions/shared/QueryParameter/limit.ts
+++ nodes/Infinigate/descriptions/shared/QueryParameter/limit.ts
@@ -2,11 +2,23 @@ import { type INodeProperties } from 'n8n-workflow';
 
 export const limitQueryParameters: INodeProperties[] = [
 	{
+		name: 'useLimit',
+		displayName: 'Use Limit',
+		description: 'Whether to use limit for the request',
+		type: 'boolean',
+		default: false,
+	},
+	{
 		name: 'Take',
 		displayName: 'Take',
 		type: 'number',
 		default: '',
 		description: 'Number of items to take',
+		displayOptions: {
+			show: {
+				useLimit: [true],
+			},
+		},
 		routing: {
 			send: {
 				type: 'query',
@@ -20,6 +32,11 @@ export const limitQueryParameters: INodeProperties[] = [
 		type: 'number',
 		default: '',
 		description: 'Number of items to skip',
+		displayOptions: {
+			show: {
+				useLimit: [true],
+			},
+		},
 		routing: {
 			send: {
 				type: 'query',
